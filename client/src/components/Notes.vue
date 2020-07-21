1595698c381477dd65b169d25330347a24b628c9
Mike Miller <mmiller@levelsbeyond.com>
2020-07-20 21:59:21 -0600

    clean up, almsot docker

--- client/src/components/Notes.vue
+++ client/src/components/Notes.vue
@@ -9,94 +9,100 @@
         <br/><br/>
         <table class="table table-hover">
           <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Note</th>
-              <th></th>
-            </tr>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Note</th>
+            <th></th>
+          </tr>
           </thead>
           <tbody>
-            <tr v-for="(note, index) in notes" :key="index">
-              <td>{{ note.id }}</td>
-              <td>{{ note.body }}</td>
-              <td>
-                <div class="btn-group" role="group">
-                  <button
-                    type="button"
-                    class="btn btn-danger btn-sm"
-                    @click="onDeleteNote(note)">
-                    Delete
-                  </button>
-                </div>
-              </td>
-            </tr>
+          <tr v-for="(note, index) in notes" :key="index">
+            <td>{{ note.id }}</td>
+            <td>{{ note.body }}</td>
+            <td>
+              <div class="btn-group" role="group">
+                <button
+                  type="button"
+                  class="btn btn-danger btn-sm"
+                  @click="onDeleteNote(note)">
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
           </tbody>
         </table>
       </div>
     </div>
     <b-modal ref="addNoteModal"
-    id="note-modal"
-    title="Add a new note"
-    hide-footer>
-    <b-form @submit="onSubmit" @reset="onReset" class="w-100">
-      <b-form-group id="form-body-group"
-      label="Note:"
-      label-for="form-note-input">
-      <b-form-input id="form-note-input"
-      type="text"
-      v-model="addNoteForm.body"
-      required
-      placeholder="Enter note">
-    </b-form-input>
-  </b-form-group>
-  <b-button-group>
-    <b-button type="submit" variant="primary">Submit</b-button>
-    <b-button type="reset" variant="danger">Reset</b-button>
-  </b-button-group>
-</b-form>
-</b-modal>
-</div>
+             id="note-modal"
+             title="Add a new note"
+             hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group id="form-body-group"
+                      label="Note:"
+                      label-for="form-note-input">
+          <b-form-input id="form-note-input"
+                        type="text"
+                        v-model="addNoteForm.body"
+                        required
+                        placeholder="Enter note">
+          </b-form-input>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Submit</b-button>
+          <b-button type="reset" variant="danger">Reset</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
+  </div>
 </template>
 
 <script>
   import axios from "axios";
-  
+
   export default {
     data() {
       return {
         notes: [],
         addNoteForm: {
-        body: '',
-      },
+          body: '',
+        },
       };
     },
     methods: {
       getNotes() {
-        const path = "http://localhost:5000/api/notes";
+        //const path = "http://localhost:5000/api/notes";
+        const host = process.env.VUE_APP_FLASK_API;
+        const path = "http://" + host + ":5000/api/notes";
         axios
-        .get(path)
-        .then(res => {
-          this.notes = res.data.notes;
-        })
-        .catch(error => {
-          // eslint-disable-next-line
-          console.error(error);
-        });
+          .get(path)
+          .then(res => {
+            this.notes = res.data.notes;
+          })
+          .catch(error => {
+            // eslint-disable-next-line
+            console.error(error);
+          });
       },
       addNote(payload) {
-        const path = 'http://localhost:5000/api/notes';
+        //const path = 'http://localhost:5000/api/notes';
+        const host = process.env.VUE_APP_FLASK_API;
+        const path = "http://" + host + ":5000/api/notes";
         axios.post(path, payload)
-        .then(() => {
-          this.getNotes();
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.log(error);
-          this.getNotes();
-        });
+          .then(() => {
+            this.getNotes();
+          })
+          .catch((error) => {
+            // eslint-disable-next-line
+            console.log(error);
+            this.getNotes();
+          });
       },
       removeNote(noteID) {
-        const path = `http://localhost:5000/api/notes/${noteID}`;
+        //const path = `http://localhost:5000/api/notes/${noteID}`;
+        const host = process.env.VUE_APP_FLASK_API;
+        const path = "http://" + host + `:5000/api/notes/${noteID}`;
         axios.delete(path)
           .then(() => {
             this.getNotes();
@@ -130,8 +136,8 @@
         this.initForm();
       },
     },
-      created() {
-        this.getNotes();
-      },
-    };
+    created() {
+      this.getNotes();
+    },
+  };
 </script>
