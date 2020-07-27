cfd2f37d9430faf8ed0a841df36b53ed9c336064
Mike Miller <mmiller@levelsbeyond.com>
2020-07-26 19:48:34 -0600

    tweaks

--- api/tests/testAPI.py
+++ api/tests/testAPI.py
@@ -1,5 +1,5 @@
-from app import app
 from flask import json
+from app import app
 
 # Run this in the api/ dir with
 # python -m pytest --rootdir=`pwd` tests/api-test.py -v
@@ -9,31 +9,31 @@ def test_add_note():
     response = app.test_client().post(
         "/api/v1.0/notes",
         data=json.dumps({"body": "Thnk of more test notes!"}),
-        content_type="application/json",
+        content_type="application/json"
     )
 
     assert response.status_code == 201
 
 
 def test_get_one_note():
-    response = app.test_client().get("/api/v1.0/notes/1",)
+    response = app.test_client().get("/api/v1.0/notes/1")
 
     assert response.status_code == 200
 
 
 def test_get_one_note_bad():
-    response = app.test_client().get("/api/v1.0/notes/10",)
+    response = app.test_client().get("/api/v1.0/notes/10")
 
     assert response.status_code == 404
 
 
 def test_get_all_notes():
-    response = app.test_client().get("/api/v1.0/notes",)
+    response = app.test_client().get("/api/v1.0/notes")
 
     assert response.status_code == 200
 
 
 def test_delete_note():
-    response = app.test_client().delete("/api/v1.0/notes/1",)
+    response = app.test_client().delete("/api/v1.0/notes/1")
 
     assert response.status_code == 200
